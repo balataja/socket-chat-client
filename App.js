73b3b2a5df66fa8004108c7924acde4c9783d280
James Balata <Balataja@Jamess-MacBook-Pro.local>
2017-10-09 20:08:44 -0400

    socket.io installed and communicating with socket-chat-backend

--- App.js
+++ App.js
@@ -1,14 +1,46 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TextInput, ScrollView } from 'react-native';
+import SocketIOClient from 'socket.io-client'
 
 export default class App extends React.Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      text: ''
+    }
+
+    this.onMessageSubmit = this.onMessageSubmit.bind(this)
+    this.socket = SocketIOClient('http://localhost:3000')
+    this.socket.on('connect', function () {
+      console.log('Connected from React Native')
+    })
+    this.socket.on('newMessage', (message) => {
+      this.setState({text: `${this.state.text}\nFrom Server: ${message.text}`})      
+    })
+  }
+
+  onMessageSubmit(e) {
+    this.setState({text: `${this.state.text}\n${e.nativeEvent.text}`})
+    this.socket.emit('createMessage', {
+      from: 'User',
+      text: e.nativeEvent.text
+    })
+  }
+
   render() {
     return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
-        <Text>This is pretty neat!</Text>
+      <View style={{flex: 1,  
+          flexDirection: 'column',
+          justifyContent: 'flex-start'}}>
+        <ScrollView>
+          <Text>Scroll Me!</Text>
+          <Text>{this.state.text}</Text>
+        </ScrollView>
+        <TextInput 
+          placeholder="Text Message"
+          onSubmitEditing={(newText) => this.onMessageSubmit(newText)}
+        />
       </View>
     );
   }
